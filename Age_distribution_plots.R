56ce28300f6dd8940691b4bb61a89cbcf3d29a00
Yasmijnb <yasmijn.balder@hotmail.com>
2021-02-02 11:14:02 +0100

    New script: Age_distribution_plots.R This plots the age distribution per sex. Also tertiles of the age distribution are used to create young/old groups per sex.

---
+++ Age_distribution_plots.R
@@ -0,0 +1,81 @@
+###############################################################################
+
+# Yasmijn Balder
+# 25-01-2021
+
+# Investigate patient characteristics/demographics
+
+# Output
+# Plots showing number of individuals per age (one plot for men, one for women)
+
+###############################################################################
+
+# Load packages
+
+library(ggplot2)
+
+###############################################################################
+
+# Load data
+
+# Set working directory
+setwd("C:/Users/Yasmijn/Documents/School/WUR/SSB-80324 - Second Thesis/")
+# Load data
+data <- read.csv("Data/Lipids_age_sex.csv", check.names = FALSE)
+
+###############################################################################
+
+# Create histograms of the age distribution per gender 
+# (figure 1 of Vignoli et. al. 2017)
+
+# Women
+hist(data$Age[which(data$Gender == 2)], 
+     breaks = length(unique(data$Age[which(data$Gender == 2)])), 
+     xlab = 'Age (years)', ylab = 'Number of individuals', col = 'magenta',
+     main = 'Age distribution of women')
+abline(v = 37, col = 'blue')
+abline(v = 48, col = 'blue')
+
+ggplot(data[which(data$Gender == 2),], aes(x = Age)) + 
+  geom_histogram(binwidth = 1, color = 'white', fill = 'magenta') + 
+  labs(title = "Age distribution of women",
+       x = "Age (years)", y = "Number of individuals") + 
+  scale_x_continuous(breaks = seq(range(data$Age)[1], range(data$Age)[2], 1)) + 
+  scale_y_continuous(breaks = seq(0, 10, 2)) +
+  theme(axis.text.x = element_text(angle = 90)) +
+  theme(panel.border = element_blank(),
+        panel.grid.major.x = element_blank(), panel.grid.minor.x = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"))
+
+# Men
+hist(data$Age[which(data$Gender == 1)], 
+     breaks = length(unique(data$Age[which(data$Gender == 1)])), 
+     xlab = 'Age (years)', ylab = 'Number of individuals', col = 'blue',
+     main = 'Age distribution of men')
+abline(v = 35, col = 'red')
+abline(v = 45, col = 'red')
+
+ggplot(data[which(data$Gender == 1),], aes(x = Age)) + 
+  geom_histogram(binwidth = 1, color = 'white', fill = 'cyan') + 
+  labs(title = "Age distribution of men",
+       x = "Age (years)", y = "Number of individuals") + 
+  scale_x_continuous(breaks = seq(range(data$Age)[1], range(data$Age)[2], 1)) + 
+  scale_y_continuous(breaks = seq(0, 30, 5)) +
+  theme(axis.text.x = element_text(angle = 90)) +
+  theme(panel.border = element_blank(),
+        panel.grid.major.x = element_blank(), panel.grid.minor.x = element_blank(),
+        panel.background = element_blank(), axis.line = element_line(colour = "black"))
+
+###############################################################################
+
+# Find groups for young and old using quantiles
+
+# Women
+quantile(data$Age[which(data$Gender == 2)], probs = seq(0, 1, 1/3))
+# Young < 37
+# Old   > 48
+
+# Men
+quantile(data$Age[which(data$Gender == 1)], probs = seq(0, 1, 1/3))
+# Young < 35
+# Old   > 45
