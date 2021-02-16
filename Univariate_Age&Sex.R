ef35277b23ff09607672bbc3e51bfae783d0e6bb
Yasmijnb <yasmijn.balder@hotmail.com>
2021-02-16 15:41:16 +0100

    Use only main fractions

--- Univariate_Age&Sex.R
+++ Univariate_Age&Sex.R
@@ -3,7 +3,7 @@
 # Yasmijn Balder
 # 11-02-2021
 
-# Univariate data analysis between:
+# Univariate data analysis using only main fractions
     # Men and Women
     # Young and Old
     # Young Women and Old women
@@ -43,7 +43,8 @@ men <- data[which(data$Gender=='man'),]
 women <- data[which(data$Gender=='woman'),]
 
 # Perform a Wilcoxon rank-sum test for every lipid comparing men and women
-for (lipid in 4:ncol(data)) {
+# for (lipid in 4:ncol(data)) { # All lipid measurements
+for (lipid in 23:43) {
   wil.test <- wilcox.test(men[,lipid], women[,lipid], 
                           alternative = "two.sided", conf.int = T)
   P.values <- c(P.values, wil.test$p.value)
@@ -59,7 +60,8 @@ young <- data[which(data$Age < quantile(data$Age, probs = 1/3)),]
 old <- data[which(data$Age > quantile(data$Age, probs = 2/3)),]
 
 # Perform a Wilcoxon rank-sum test for every lipid comparing young and old
-for (lipid in 4:ncol(data)) {
+for (lipid in 23:43) {
+# for (lipid in 4:ncol(data)) { # All lipid measurements
   wil.test <- wilcox.test(young[,lipid], old[,lipid], 
                           alternative = "two.sided", conf.int = T)
   P.values <- c(P.values, wil.test$p.value)
@@ -75,7 +77,8 @@ young.men <- men[men$Age < quantile(men$Age, probs = 1/3),]
 old.men <- men[men$Age > quantile(men$Age, probs = 2/3),]
 
 # Perform a Wilcoxon rank-sum test for every lipid comparing young and old
-for (lipid in 4:ncol(data)) {
+# for (lipid in 4:ncol(data)) { # All lipid measurements
+for (lipid in 23:43) {
   wil.test <- wilcox.test(young.men[,lipid], old.men[,lipid], 
                           alternative = "two.sided", conf.int = T)
   P.values <- c(P.values, wil.test$p.value)
@@ -91,7 +94,8 @@ young.women <- women[women$Age < quantile(women$Age, probs = 1/3),]
 old.women <- women[women$Age > quantile(women$Age, probs = 2/3),]
 
 # Perform a Wilcoxon rank-sum test for every lipid comparing young and old
-for (lipid in 4:ncol(data)) {
+# for (lipid in 4:ncol(data)) { # All lipid measurements
+for (lipid in 23:43) {
   # One sample contains only zero's which the test cannot handle
   if (sum(young.women[,lipid]) == 0) {
     P.values <- c(P.values, 1)
@@ -118,15 +122,17 @@ directions[which(adjusted.P.values <= 0.05 & shifts < 0)] <- '+' # Reverse is in
 directions[which(adjusted.P.values <= 0.05 & shifts > 0)] <- '-' # Reverse is intentional!!!
 
 # Make an empty dataframe to store the results
-wilcoxon.summary <- matrix(ncol = 4, nrow = 114)
+# wilcoxon.summary <- matrix(ncol = 4, nrow = 114) # All lipid measurements
+wilcoxon.summary <- matrix(ncol = 4, nrow = 21)
 colnames(wilcoxon.summary) <- c('Sex', 'Age', 'Women', 'Men')
-rownames(wilcoxon.summary) <- colnames(data)[4:117]
+# rownames(wilcoxon.summary) <- colnames(data)[4:117] # All lipid measurements
+rownames(wilcoxon.summary) <- colnames(data)[23:43]
 
 # Fill in the directions in the summary table
-wilcoxon.summary[1:114,1] <- directions[1:114]
-wilcoxon.summary[1:114,2] <- directions[115:228]
-wilcoxon.summary[1:114,3] <- directions[229:342]
-wilcoxon.summary[1:114,4] <- directions[343:456]
+wilcoxon.summary[1:21,1] <- directions[1:21]
+wilcoxon.summary[1:21,2] <- directions[22:42]
+wilcoxon.summary[1:21,3] <- directions[43:63]
+wilcoxon.summary[1:21,4] <- directions[64:84]
 
 # How many lipids are significant before and after?
 length(which(P.values <= 0.05))
