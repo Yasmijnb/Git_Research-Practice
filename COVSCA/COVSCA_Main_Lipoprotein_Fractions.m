0743c9fcea788a014e0bf1216860329414cd0753
Yasmijnb <yasmijn.balder@hotmail.com>
2021-02-22 17:00:41 +0100

    Lipids as x-labels

--- COVSCA/COVSCA_Main_Lipoprotein_Fractions.m
+++ COVSCA/COVSCA_Main_Lipoprotein_Fractions.m
@@ -2,14 +2,14 @@ clear all
 close all
 
 %% Retrieve the adjacency matrices
-Ad1 = readmatrix('Adjacency_matrix_men.xlsx');
-Ad2 = readmatrix('Adjacency_matrix_women.xlsx');
-Ad3 = readmatrix('Adjacency_matrix_young.xlsx');
-Ad4 = readmatrix('Adjacency_matrix_old.xlsx');
-Ad5 = readmatrix('Adjacency_matrix_young_men.xlsx');
-Ad6 = readmatrix('Adjacency_matrix_old_men.xlsx');
-Ad7 = readmatrix('Adjacency_matrix_young_women.xlsx');
-Ad8 = readmatrix('Adjacency_matrix_old_women.xlsx');
+Ad1 = readmatrix('..\Data\Adjacency_matrix_men.xlsx');
+Ad2 = readmatrix('..\Data\Adjacency_matrix_women.xlsx');
+Ad3 = readmatrix('..\Data\Adjacency_matrix_young.xlsx');
+Ad4 = readmatrix('..\Data\Adjacency_matrix_old.xlsx');
+Ad5 = readmatrix('..\Data\Adjacency_matrix_young_men.xlsx');
+Ad6 = readmatrix('..\Data\Adjacency_matrix_old_men.xlsx');
+Ad7 = readmatrix('..\Data\Adjacency_matrix_young_women.xlsx');
+Ad8 = readmatrix('..\Data\Adjacency_matrix_old_women.xlsx');
 
 %% Make input for COVSCA
 COVSCAinput = [Ad1 Ad2 Ad3 Ad4 Ad5 Ad6 Ad7 Ad8];
@@ -19,7 +19,7 @@ COVSCAinput = [Ad1 Ad2 Ad3 Ad4 Ad5 Ad6 Ad7 Ad8];
 nanal = 1000; % Larger number will give more accurate resutls
 
 % The number of loadings for each component
-Q = [3 3]';
+Q = [2 2]';
 % The number of dimensions
 L = length(Q);
 
@@ -41,7 +41,8 @@ plot(scores(1,1),scores(1,2),'b.', ...
     scores(7,1),scores(7,2),'r.', ...
     scores(8,1),scores(8,2),'r.', ...
     'MarkerSize',22);
-text(scores(:,1),scores(:,2),labels,'VerticalAlignment','bottom','HorizontalAlignment','left')
+text(scores(:,1),scores(:,2),labels,'VerticalAlignment','bottom',...
+    'HorizontalAlignment','left')
 leg = legend('men', 'women', 'all');
 legtitle = get(leg, 'Title');
 set(legtitle,'String','Gender')
@@ -50,18 +51,49 @@ ylabel('2nd COVSCA component','FontSize',13);
 title('COVSCA scores (weights)','FontSize',16);
 
 %% Plot scores and Loadings
+
+% Assign the variable names
+variable_labels = {'Triglycerides, VLDL', 'Triglycerides, IDL', ...
+    'Triglycerides, LDL', 'Triglycerides, HDL', 'Cholesterol, VLDL', ...
+    'Cholesterol, IDL', ' Cholesterol, LDL', 'Cholesterol, HDL', ...
+    'Free Cholesterol, VLDL', 'Free Cholesterol, IDL', ...
+    'Free Cholesterol, LDL', 'Free Cholesterol, HDL', ...
+    'Phospholipids, VLDL', 'Phospholipids, IDL', 'Phospholipids, LDL', ...
+    'Phospholipids, HDL', 'Apo-A1, HDL', 'Apo-A2, HDL', 'Apo-B, VLDL', ...
+    'Apo-B, IDL', ' Apo-B, LDL'};
+
+% Show two loadings at a time
 figure(2)
-subplot(4,1,1)
+set(gcf, 'color', 'w');
+title('COVSCA loadings','FontSize',16);
+xlabel('Variables','FontSize',13);
+% First
+subplot(2,1,1)
 bar(loadings(:,1)');
 ylabel('Loadings 1st comp','FontSize',13);
-title('COVSCA loadings','FontSize',16);
-subplot(4,1,2)
+set(gca, 'xtick', [1:21], 'xticklabel', variable_labels);
+xtickangle(45);
+% Second
+subplot(2,1,2)
 bar(loadings(:,2)');
 ylabel('Loadings 2nd comp','FontSize',13);
-subplot(4,1,3)
-bar(loadings(:,3)');
-ylabel('Loadings 3rd comp','FontSize',13);
-subplot(4,1,4)
-bar(loadings(:,4)');
+set(gca, 'xtick', [1:21], 'xticklabel', variable_labels);
+xtickangle(45);
+
+% Show two loadings at a time
+figure(3)
+set(gcf, 'color', 'w');
+title('COVSCA loadings','FontSize',16);
 xlabel('Variables','FontSize',13);
-ylabel('Loadings 4th comp','FontSize',13);
+% First
+subplot(2,1,1)
+bar(loadings(:,3)');
+ylabel('Loadings 1st comp','FontSize',13);
+set(gca, 'xtick', [1:21], 'xticklabel', variable_labels);
+xtickangle(45);
+% Second
+subplot(2,1,2)
+bar(loadings(:,4)');
+ylabel('Loadings 2nd comp','FontSize',13);
+set(gca, 'xtick', [1:21], 'xticklabel', variable_labels);
+xtickangle(45);
