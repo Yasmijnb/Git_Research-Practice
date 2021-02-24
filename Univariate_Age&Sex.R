2b51d1933e05ec85ff96ccb9bae93dccfcfb7cff
Yasmijnb <yasmijn.balder@hotmail.com>
2021-02-24 13:57:28 +0100

    Chanced significance level to p < 0.01

--- Univariate_Age&Sex.R
+++ Univariate_Age&Sex.R
@@ -118,8 +118,8 @@ adjusted.P.values <- p.adjust(P.values, method = 'bonferroni')
 # Make a vector with the directions of the shifts
 directions <- rep('', length(adjusted.P.values))
 # + means a higher value for women (Sex), old (Age, Women, Men)
-directions[which(adjusted.P.values <= 0.05 & shifts < 0)] <- '+' # Reverse is intentional!!!
-directions[which(adjusted.P.values <= 0.05 & shifts > 0)] <- '-' # Reverse is intentional!!!
+directions[which(adjusted.P.values <= 0.01 & shifts < 0)] <- '+' # Reverse is intentional!!!
+directions[which(adjusted.P.values <= 0.01 & shifts > 0)] <- '-' # Reverse is intentional!!!
 
 # Make an empty dataframe to store the results
 # wilcoxon.summary <- matrix(ncol = 4, nrow = 114) # All lipid measurements
@@ -135,11 +135,11 @@ wilcoxon.summary[1:21,3] <- directions[43:63]
 wilcoxon.summary[1:21,4] <- directions[64:84]
 
 # How many lipids are significant before and after?
-length(which(P.values <= 0.05))
-length(which(adjusted.P.values <= 0.05))
+length(which(P.values <= 0.01))
+length(which(adjusted.P.values <= 0.01))
 # Plot the original and adjusted p.values to see the effect of the adjustment
 plot(P.values, adjusted.P.values)
-abline(h = 0.05, col = 'red'); abline(v = 0.05, col = 'red')
+abline(h = 0.01, col = 'red'); abline(v = 0.01, col = 'red')
 
 # Set different working directory results
 setwd("C:/Users/Yasmijn/Documents/School/WUR/SSB-80324 - Second Thesis/Git_Research-Practice/Results")
