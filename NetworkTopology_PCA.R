8742f810fa121baed6635ab4588498b778931df2
Yasmijnb <yasmijn.balder@hotmail.com>
2021-03-12 14:52:48 +0100

    Plot lipid number

--- NetworkTopology_PCA.R
+++ NetworkTopology_PCA.R
@@ -34,25 +34,25 @@ old.women <- read.csv('C:/Users/Yasmijn/Documents/School/WUR/SSB-80324 - Second 
 AGE <- rbind(young, old)
 AGE$network <- c(rep('young', nrow(young)), rep('old', nrow(old)))
 AGE.pca <- prcomp(AGE[,c(2:7, 12:13, 16, 20:21)], scale = T)
-autoplot(AGE.pca, data = AGE, colour = 'network', main = 'Age')
+autoplot(AGE.pca, data = AGE, colour = 'network', main = 'Age', label = T, shape = F)
 
 # Make a PCA for sex
 SEX <- rbind(men, women)
 SEX$network <- c(rep('men', nrow(men)), rep('women', nrow(women)))
 SEX.pca <- prcomp(SEX[,c(2:7, 12:13, 16, 20:21)], scale = T)
-autoplot(SEX.pca, data = SEX, colour = 'network', main = 'Sex')
+autoplot(SEX.pca, data = SEX, colour = 'network', main = 'Sex', label = T, shape = F)
 
 # Make a PCA for men
 MEN <- rbind(young.men, old.men)
 MEN$network <- c(rep('young men', nrow(young.men)), rep('old men', nrow(old.men)))
 MEN.pca <- prcomp(MEN[,c(2:7, 12:13, 16, 20:21)], scale = T)
-autoplot(MEN.pca, data = MEN, colour = 'network', main = 'Men')
+autoplot(MEN.pca, data = MEN, colour = 'network', main = 'Men', label = T, shape = F)
 
 # Make a PCA for women
 WOMEN <- rbind(young, old)
 WOMEN$network <- c(rep('young women', nrow(young.women)), rep('old women', nrow(old.women)))
 WOMEN.pca <- prcomp(WOMEN[,c(2:7, 12:13, 16, 20:21)], scale = T)
-autoplot(WOMEN.pca, data = WOMEN, colour = 'network', main = 'Women')
+autoplot(WOMEN.pca, data = WOMEN, colour = 'network', main = 'Women', label = T, shape = F)
 
 ###############################################################################
 
