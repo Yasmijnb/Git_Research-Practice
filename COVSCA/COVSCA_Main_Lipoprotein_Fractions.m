d048c9b6d51a2a7639c475030ddf9b959f1d5d66
Yasmijnb <yasmijn.balder@hotmail.com>
2021-03-15 17:00:30 +0100

    Don't include all young or all old

--- COVSCA/COVSCA_Main_Lipoprotein_Fractions.m
+++ COVSCA/COVSCA_Main_Lipoprotein_Fractions.m
@@ -2,17 +2,17 @@ clear all
 close all
 
 %% Retrieve the adjacency matrices
-Ad1 = readmatrix('..\Data\Adjacency_matrix_men.xlsx');
-Ad2 = readmatrix('..\Data\Adjacency_matrix_women.xlsx');
-Ad3 = readmatrix('..\Data\Adjacency_matrix_young.xlsx');
-Ad4 = readmatrix('..\Data\Adjacency_matrix_old.xlsx');
-Ad5 = readmatrix('..\Data\Adjacency_matrix_young_men.xlsx');
-Ad6 = readmatrix('..\Data\Adjacency_matrix_old_men.xlsx');
-Ad7 = readmatrix('..\Data\Adjacency_matrix_young_women.xlsx');
-Ad8 = readmatrix('..\Data\Adjacency_matrix_old_women.xlsx');
+Ad1 = readmatrix('\Adjacency_matrix_men.xlsx');
+Ad2 = readmatrix('\Adjacency_matrix_women.xlsx');
+% Ad3 = readmatrix('\Adjacency_matrix_young.xlsx');
+% Ad4 = readmatrix('\Adjacency_matrix_old.xlsx');
+Ad5 = readmatrix('\Adjacency_matrix_youngmen.xlsx');
+Ad6 = readmatrix('\Adjacency_matrix_oldmen.xlsx');
+Ad7 = readmatrix('\Adjacency_matrix_youngwomen.xlsx');
+Ad8 = readmatrix('\Adjacency_matrix_oldwomen.xlsx');
 
 %% Make input for COVSCA
-COVSCAinput = [Ad1 Ad2 Ad3 Ad4 Ad5 Ad6 Ad7 Ad8];
+COVSCAinput = [Ad1 Ad2 Ad5 Ad6 Ad7 Ad8];
 
 %% Input parameters
 % Number of analyses
@@ -31,19 +31,17 @@ disp(fp) % Value between 0 and 100
 
 %% Plot scores and Loadings
 figure(1)
-labels = {'all','all','young','old','young','old','young','old'};
+labels = {'all','all','young','old','young','old'};
 plot(scores(1,1),scores(1,2),'b.', ...
     scores(2,1),scores(2,2),'r.', ...
-    scores(3,1),scores(3,2),'k.', ...
-    scores(4,1),scores(4,2),'k.', ...
-    scores(5,1),scores(5,2),'b.', ...
-    scores(6,1),scores(6,2),'b.', ...
-    scores(7,1),scores(7,2),'r.', ...
-    scores(8,1),scores(8,2),'r.', ...
+    scores(3,1),scores(3,2),'b.', ...
+    scores(4,1),scores(4,2),'b.', ...
+    scores(5,1),scores(5,2),'r.', ...
+    scores(6,1),scores(6,2),'r.', ...
     'MarkerSize',22);
 text(scores(:,1),scores(:,2),labels,'VerticalAlignment','bottom',...
     'HorizontalAlignment','left')
-leg = legend('men', 'women', 'all');
+leg = legend('men', 'women');
 legtitle = get(leg, 'Title');
 set(legtitle,'String','Gender')
 xlabel('1st COVSCA component','FontSize',13);
