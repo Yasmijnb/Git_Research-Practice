022cc964f6e43e1e19783d5ec2a4518db097e8cb
Yasmijnb <yasmijn.balder@hotmail.com>
2021-03-17 10:48:47 +0100

    Adjust for my data

--- Diffcon_pvalue_plot.R
+++ Diffcon_pvalue_plot.R
@@ -12,6 +12,7 @@ diffcon_plot <- function(res_path){
   require(ggplot2)
   require(janitor)
   require(ggrepel)
+  require(Hmisc)
   
   merge.multiple <- function(x, ..., by = "row.names") {
     #
@@ -32,7 +33,7 @@ diffcon_plot <- function(res_path){
   }
   
   # Read in fatty acid levels for plotting
-  fa_labels <- read.table('FAclass_subset.txt', check.names = F, header = T, sep='\t', row.names = 1)
+  fa_labels <- read.table('Lipid_groups.txt', check.names = F, header = T, sep='\t', row.names = 1)
   
   # Gather files from directory
   files <- list.files(pattern = "*.rds")
@@ -42,20 +43,20 @@ diffcon_plot <- function(res_path){
     data <- readRDS(file)
     
     # Get specific comparison name
-    comp <- sub("\\_diffcon_subset_1502_maxperm2000.rds$", "", file)
+    comp <- sub("\\.pclrc.rds$", "", file)
     print(comp)
     pvalues <- data$Pval_adj
     
     # Print statements to get better overview of results
     print(comp)
-    print('These are adjusted p-values')
+    print('These are the adjusted p-values')
     print(pvalues)
-    print('These are p-values')
+    print('These are the p-values')
     print(data$Pval)
     
     
     #  Calculate -log10 of adjusted p-values
-    pvalues <- data.frame(log10(pvalues)) *(-1)
+    pvalues <- data.frame(log10(pvalues)) * (-1)
     diffcon <- data.frame(data$Diff_Conn)
     
     # Merge dataframes by rowname
@@ -74,10 +75,10 @@ diffcon_plot <- function(res_path){
                                 "ApoB_LDL")
     
     # Plot difference in connectivity with significance
-    myplot <- ggplot(diffcon_data, aes(x = data_diff_conn, y = log10_pvalues, color = type)) +
-      geom_point(aes(shape = type, color = type), size=6) + 
+    myplot <- ggplot(diffcon_data, aes(x = data_diff_conn, y = log10_pvalues, color = group)) +
+      geom_point(aes(shape = group, color = group), size=6) + 
       geom_text_repel(aes(label=ifelse(log10_pvalues>0.01, as.character(rownames(diffcon_data)), '')), color='black', size=4, nudge_y = -0.06, hjust = 0.6) +
-      scale_shape_manual(name = 'Lipid Type', values = c(15, 17, 19)) + scale_color_manual(name = 'Lipid Main Fractions', values=c("#0033cc", "#ffcc00", "#808080")) +
+      scale_shape_manual(name = 'Lipid Type', values = c(15:21)) + scale_color_manual(name = 'Lipid Type', values=c("#0073C2", "#EFC000", "#868686", "#CD534C", "#7AA6DC", "#003C6799", "#8F7700")) +
       labs(x = "Difference in Connectivity", y = '-log10(P-value)') +
       ggtitle(comp) + 
       ylim(min(diffcon_data$log10_pvalues)-0.1, max(diffcon_data$log10_pvalues)+0.1) + xlim(min(diffcon_data$data_diff_conn), max(diffcon_data$data_diff_conn)+0.25) +
@@ -89,5 +90,7 @@ diffcon_plot <- function(res_path){
   
   
 }
+
+setwd("C:/Users/Yasmijn/Documents/School/WUR/SSB-80324 - Second Thesis/Git_Research-Practice/Results/")
 
 diffcon_plot("C:/Users/Yasmijn/Documents/School/WUR/SSB-80324 - Second Thesis/Git_Research-Practice/Results/")
