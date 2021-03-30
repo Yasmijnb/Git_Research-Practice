6de446ea158c386507b49886de5c0f59f04678d3
Yasmijnb <yasmijn.balder@hotmail.com>
2021-03-30 11:19:00 +0200

    Remove old methods

--- NetworkTopology_HubNodes.R
+++ NetworkTopology_HubNodes.R
@@ -37,45 +37,7 @@ networks <- list('men' = men, 'women' = women, 'young' = young, 'old' = old,
                  'young.men' = young.men, 'old.men' = old.men, 
                  'young.women' = young.women, 'old.women' = old.women)
 
-# Go through all networks
-name <- 1
-for (network in networks) {
-  for (lipid in 1:nrow(network)) {
-    # Get the mean and sd of the clustering coefficient in this network
-    mean.clust <- mean(network$ClusteringCoefficient)
-    sd.clust <- sd(network$ClusteringCoefficient)
-    # Get the threshold for a hub by adding 1 sd to the mean
-    thr.clust <- mean.clust - sd.clust
-    # Get the mean and sd of the degree in this network
-    mean.degree <- mean(network$Degree)
-    sd.degree <- sd(network$Degree)
-    # Get the threshold for a hub by adding 1 sd to the mean
-    thr.degree <- mean.degree + sd.degree
-    # Find the hub nodes
-    if (network$ClusteringCoefficient[lipid] < thr.clust & network$Degree[lipid] > thr.degree) {
-      print('Found a hub node!')
-      # Add the hub node to the table
-      hub.nodes <- rbind(hub.nodes, c(names(networks)[name], network$name[lipid]))
-    }
-  }
-  name <- name+ 1
-}
-
-# Give the hub node table column names
-colnames(hub.nodes) <- c('Network','Lipid')
-
-View(hub.nodes)
-
 ###############################################################################
-
-# What is the difference between the cc's per network?
-cc.diff <- data.frame(row.names = women$name)
-cc.diff$sex <- women$ClusteringCoefficient - men$ClusteringCoefficient
-cc.diff$age <- young$ClusteringCoefficient - old$ClusteringCoefficient
-cc.diff$men <- young.men$ClusteringCoefficient - old.men$ClusteringCoefficient
-cc.diff$women <- young.women$ClusteringCoefficient - old.women$ClusteringCoefficient
-
-View(cc.diff)
 
 # Which change from < 0.03 to > 0.03?
 cc.thrs <- data.frame(row.names = women$name)
