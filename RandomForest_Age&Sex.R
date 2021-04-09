bb9678b7f90a34d4d5d2fd640e4247efeeda9ea9
Yasmijnb <yasmijn.balder@hotmail.com>
2021-04-09 09:46:24 +0200

    Use rfPermute for variable importance instead of a single random forest

--- RandomForest_Age&Sex.R
+++ RandomForest_Age&Sex.R
@@ -35,20 +35,23 @@ sex.forest$ModelStatistics
 
 ###############################################################################
 
-## Age
+## Men
+
+# Use only men
+men <- data[which(data$Gender == 'man'),]
 
 # Make young and old groups
-# age_groups <- rep(0, nrow(data))
-# age_groups[which(data$Age < quantile(data$Age, probs = 1/3))] <- 'young'
-# age_groups[which(data$Age > quantile(data$Age, probs = 2/3))] <- 'old'
-# 
-# # Use only young and old, not the middle
-# age.data <- data[which(age_groups == 'young' | age_groups == 'old'),]
-# group.data <- age_groups[which(age_groups == 'young' | age_groups == 'old')]
-# 
-# age.forest <- RForest(x.data = age.data[,23:43], y.class = group.data, 
-#                       unbalance = FALSE)
-# age.forest$ModelStatistics
+men$AgeGroup <- rep(0, nrow(men))
+men$AgeGroup[which(men$Age < quantile(men$Age, probs = 1/3))] <- 'young'
+men$AgeGroup[which(men$Age > quantile(men$Age, probs = 2/3))] <- 'old'
+
+# Use only young and old, not the middle
+men <- men[which(men$AgeGroup == 'young' | men$AgeGroup == 'old'),]
+# men.group.data <- men.age_groups[which(men.age_groups == 'young' | men.age_groups == 'old')]
+
+men.forest <- RForest(x.data = men[,23:43], y.class = men$AgeGroup, 
+                      unbalance = FALSE)
+men.forest$ModelStatistics
 
 ###############################################################################
 
@@ -72,31 +75,10 @@ women.forest$ModelStatistics
 
 ###############################################################################
 
-## Men
-
-# Use only men
-men <- data[which(data$Gender == 'man'),]
-
-# Make young and old groups
-men$AgeGroup <- rep(0, nrow(men))
-men$AgeGroup[which(men$Age < quantile(men$Age, probs = 1/3))] <- 'young'
-men$AgeGroup[which(men$Age > quantile(men$Age, probs = 2/3))] <- 'old'
-
-# Use only young and old, not the middle
-men <- men[which(men$AgeGroup == 'young' | men$AgeGroup == 'old'),]
-# men.group.data <- men.age_groups[which(men.age_groups == 'young' | men.age_groups == 'old')]
-
-men.forest <- RForest(x.data = men[,23:43], y.class = men$AgeGroup, 
-                      unbalance = FALSE)
-men.forest$ModelStatistics
-
-###############################################################################
-
 # Print all results again
 sex.forest$ModelStatistics
-# age.forest$ModelStatistics
-women.forest$ModelStatistics
 men.forest$ModelStatistics
+women.forest$ModelStatistics
 
 ###############################################################################
 
@@ -120,43 +102,111 @@ colnames(data)[23:43] <- short.names
 n1 = sum(as.factor(data$Gender) == 'man')
 n2 = sum(as.factor(data$Gender) == 'woman')
 nsize = round(min(n1,n2)*0.85)
-# Prepare for women
-women <- data[which(data$Gender == 'woman'),]
-women$AgeGroup <- rep(0, nrow(women))
-women$AgeGroup[which(women$Age < quantile(women$Age, probs = 1/3))] <- 'young'
-women$AgeGroup[which(women$Age > quantile(women$Age, probs = 2/3))] <- 'old'
-women <- women[which(women$AgeGroup == 'young' | women$AgeGroup == 'old'),]
 # Prepare for men
 men <- data[which(data$Gender == 'man'),]
 men$AgeGroup <- rep(0, nrow(men))
 men$AgeGroup[which(men$Age < quantile(men$Age, probs = 1/3))] <- 'young'
 men$AgeGroup[which(men$Age > quantile(men$Age, probs = 2/3))] <- 'old'
 men <- men[which(men$AgeGroup == 'young' | men$AgeGroup == 'old'),]
+# Prepare for women
+women <- data[which(data$Gender == 'woman'),]
+women$AgeGroup <- rep(0, nrow(women))
+women$AgeGroup[which(women$Age < quantile(women$Age, probs = 1/3))] <- 'young'
+women$AgeGroup[which(women$Age > quantile(women$Age, probs = 2/3))] <- 'old'
+women <- women[which(women$AgeGroup == 'young' | women$AgeGroup == 'old'),]
 
-# Run single random forests
-single.sex.forest <- randomForest(data[,23:43], as.factor(data$Gender), 
-                                  importance = T, strata = as.factor(data$Gender), 
-                                  sampsize = c(nsize,nsize), replace = F,
-                                  proximity = T)
-single.women.forest <- randomForest(women[,23:43], as.factor(women$AgeGroup), 
-                                    importance = T, proximity = T)
-single.men.forest <- randomForest(men[,23:43], as.factor(men$AgeGroup), 
-                                  importance = T, proximity = T)
-
-# View importance
-varImpPlot(single.sex.forest)
-varImpPlot(single.women.forest)
-varImpPlot(single.men.forest)
-
-# View importance
-varImpPlot(single.sex.forest, sort = F)
-varImpPlot(single.women.forest, sort = F)
-varImpPlot(single.men.forest, sort = F)
+# # Run single random forests
+# single.sex.forest <- randomForest(data[,23:43], as.factor(data$Gender), 
+#                                   importance = T, strata = as.factor(data$Gender), 
+#                                   sampsize = c(nsize,nsize), replace = F,
+#                                   proximity = T)
+# single.men.forest <- randomForest(men[,23:43], as.factor(men$AgeGroup), 
+#                                   importance = T, proximity = T)
+# single.women.forest <- randomForest(women[,23:43], as.factor(women$AgeGroup), 
+#                                     importance = T, proximity = T)
+# 
+# # View importance
+# varImpPlot(single.sex.forest)
+# varImpPlot(single.men.forest)
+# varImpPlot(single.women.forest)
+# 
+# # View importance
+# varImpPlot(single.sex.forest, sort = F)
+# varImpPlot(single.men.forest, sort = F)
+# varImpPlot(single.women.forest, sort = F)
 
 ###############################################################################
 
 # Proximity distance plots
+# 
+# MDSplot(single.sex.forest, as.factor(data$Gender), palette = c('blue', 'red'))
+# MDSplot(single.men.forest, as.factor(men$AgeGroup), palette = c('darkblue', 'cyan'))
+# MDSplot(single.women.forest, as.factor(women$AgeGroup), palette = c('darkred','pink'))
 
-MDSplot(single.sex.forest, as.factor(data$Gender), palette = c('blue', 'red'))
-MDSplot(single.women.forest, as.factor(women$AgeGroup), palette = c('darkred','pink'))
-MDSplot(single.men.forest, as.factor(men$AgeGroup), palette = c('darkblue', 'cyan'))
+###############################################################################
+
+## Run rfPermuta to get the importance
+library(rfPermute)
+
+# Run random forests with permutations
+permute.sex.forest <- rfPermute(
+  # Use same random forest arguments
+  data[,23:43], as.factor(data$Gender), importance = T, proximity = T,
+  strata = as.factor(data$Gender), sampsize = c(nsize,nsize), replace = F,
+  # Use 1000 permutations 
+  nrep = 100,
+  # Use multiple cores
+  num.cores = 5)
+permute.men.forest <- rfPermute(men[,23:43], as.factor(men$AgeGroup), 
+                                importance = T, proximity = T, nrep = 100, 
+                                num.cores = 5)
+permute.women.forest <- rfPermute(women[,23:43], as.factor(women$AgeGroup), 
+                                  importance = T, proximity = T, nrep = 100,
+                                  num.cores = 5)
+
+# Prepare for gg plot
+imp.sex.forest <- as.data.frame(rp.importance(permute.sex.forest))
+imp.sex.forest$Lipids <- rownames(imp.sex.forest)
+imp.sex.forest$sig <- rep("Not significant", 21)
+imp.sex.forest$sig[which(imp.sex.forest$MeanDecreaseGini.pval < 0.05)] <- 'Significant'
+
+imp.men.forest <- as.data.frame(rp.importance(permute.men.forest))
+imp.men.forest$Lipids <- rownames(imp.men.forest)
+imp.men.forest$sig <- rep("Not significant", 21)
+imp.men.forest$sig[which(imp.men.forest$MeanDecreaseGini.pval < 0.05)] <- 'Significant'
+
+imp.women.forest <- as.data.frame(rp.importance(permute.women.forest))
+imp.women.forest$Lipids <- rownames(imp.women.forest)
+imp.women.forest$sig <- rep("Not significant", 21)
+imp.women.forest$sig[which(imp.women.forest$MeanDecreaseGini.pval < 0.05)] <- 'Significant'
+
+# Plot importance
+ggplot(data = imp.sex.forest, aes(x = MeanDecreaseGini, y = Lipids, 
+                                  fill = sig)) + 
+  geom_bar(stat = "identity") +
+  # Use nicer colours
+  scale_fill_manual(values = c("steelblue", "orange")) + 
+  # Change the axis labels
+  xlab('Mean Decrease Gini') + ylab('Lipoprotein Main Fractions') + 
+  # Make plot black and white
+  theme_bw(base_size = 17) +
+  # Remove the legend title
+  theme(legend.title = element_blank())
+
+ggplot(data = imp.men.forest, aes(x = MeanDecreaseGini, y = Lipids, 
+                                  fill = sig)) + 
+  geom_bar(stat = "identity") +
+  scale_fill_manual(values = c("steelblue", "orange")) + 
+  xlab('Mean Decrease Gini') + ylab('Lipoprotein Main Fractions') + 
+  theme_bw(base_size = 17) +
+  theme(legend.title = element_blank())
+
+ggplot(data = imp.women.forest, aes(x = MeanDecreaseGini, y = Lipids, 
+                                  fill = sig)) + 
+  geom_bar(stat = "identity") +
+  scale_fill_manual(values = c("steelblue", "orange")) + 
+  xlab('Mean Decrease Gini') + ylab('Lipoprotein Main Fractions') +
+  theme_bw(base_size = 17) +
+  theme(legend.title = element_blank()) +
+  xlim(0, 16)
+
